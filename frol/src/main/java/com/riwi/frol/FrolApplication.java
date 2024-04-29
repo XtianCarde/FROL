b5c318e9d845b34ea166cc96ffd1cf3df2f96f57
Cristian <tiancarvelasquez@gmail.com>
2024-04-29 13:12:04 -0500

    first commit

---
+++ frol/src/main/java/com/riwi/frol/FrolApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.frol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrolApplication.class, args);
+	}
+
+}
---
+++ frol/src/test/java/com/riwi/frol/FrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.frol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
