c19b42412b720ff92ccb09ed30818a5222a8f976
Luis Guillermo <155754373+LuisGuillermoF@users.noreply.github.com>
2024-05-02 19:02:06 -0500

    Revert "feature/Entity CLiente"

--- frol/src/main/java/com/riwi/frol/infrastructure/abstract_services/IClienteService.java
+++
@@ -1,8 +0,0 @@
-package com.riwi.frol.infrastructure.abstract_services;
-
-import com.riwi.frol.api.dto.request.ClienteRequest;
-import com.riwi.frol.api.dto.response.ClienteResponse;
-
-public interface IClienteService extends CrudService<ClienteRequest,ClienteResponse,String>{
-    
-}
