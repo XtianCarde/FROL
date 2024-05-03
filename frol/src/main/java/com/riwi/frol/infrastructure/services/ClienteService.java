b10003c671e81809e55d2f870ca1e0d3b2a41e0b
Luis Guiller Foronda <morganixxD@gmail.com>
2024-05-03 15:07:09 -0500

    commit

--- frol/src/main/java/com/riwi/frol/infrastructure/services/ClienteService.java
+++ frol/src/main/java/com/riwi/frol/infrastructure/services/ClienteService.java
@@ -1,10 +1,14 @@
 package com.riwi.frol.infrastructure.services;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.riwi.frol.api.dto.request.ClienteRequest;
 import com.riwi.frol.api.dto.response.ClienteResponse;
+import com.riwi.frol.domain.entities.Cliente;
+import com.riwi.frol.domain.repositories.ClienteRepository;
 import com.riwi.frol.infrastructure.abstract_services.IClienteService;
 
 import lombok.AllArgsConstructor;
@@ -13,7 +17,7 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class ClienteService implements IClienteService{
 
-    private final IClienteService objClienteService;
+    private final ClienteRepository objClienteRepository;
 
     @Override
     public void delete(String id) {
@@ -35,8 +39,19 @@ public class ClienteService implements IClienteService{
 
     @Override
     public Page<ClienteResponse> getAll(int page, int size) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+        if (page <0) page = 0;
+        PageRequest paginable = PageRequest.of(page, size);
+
+        return this.objClienteRepository.findAll(paginable).map(this::entityToResponse);
+    }
+
+    private ClienteResponse entityToResponse(Cliente cliente){
+        ClienteResponse response = new ClienteResponse();
+
+        BeanUtils.copyProperties(cliente, response);
+
+        
+        return response;
     }
 
 
